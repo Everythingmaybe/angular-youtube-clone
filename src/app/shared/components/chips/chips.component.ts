ff98440fe8763d267e287d3a9a8f5a77115ef463
Everythingmaybe <55mixxx55@gmail.com>
2022-10-19 23:42:35 +0600

    1

--- src/app/shared/components/chips/chips.component.ts
+++ src/app/shared/components/chips/chips.component.ts
@@ -1,16 +1,46 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'yt-chips',
   templateUrl: './chips.component.html',
   styleUrls: ['./chips.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChipsComponent implements OnInit {
+export class ChipsComponent {
+  public offset: number = 0;
 
-  constructor() { }
+  @ViewChild('scrollContainer', { static: true })
+  scrollContainer!: ElementRef<HTMLElement>;
 
-  ngOnInit(): void {
+  get containerWidth(): number {
+    return this.scrollContainer.nativeElement.offsetWidth;
   }
 
+  get scrollContainerWidth(): number {
+    return this.scrollContainer.nativeElement.scrollWidth;
+  }
+
+  get showPrev(): boolean {
+    return this.offset > 0;
+  }
+
+  get showNext(): boolean {
+    return this.offset + this.containerWidth < this.scrollContainerWidth;
+  }
+
+  public prev(): void {
+    this.offset = Math.max(0, this.offset - this.containerWidth);
+  }
+
+  public next(): void {
+    this.offset = Math.min(
+      this.offset + this.containerWidth,
+      this.scrollContainerWidth - this.containerWidth
+    );
+  }
 }
