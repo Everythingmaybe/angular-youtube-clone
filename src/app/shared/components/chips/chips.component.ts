4ef3bbcd5f98f46604d3ea1b2bd508ade9f63f4f
Everythingmaybe <55mixxx55@gmail.com>
2022-10-21 19:55:52 +0600

    Change style

--- src/app/shared/components/chips/chips.component.ts
+++ src/app/shared/components/chips/chips.component.ts
@@ -17,6 +17,12 @@ export class ChipsComponent {
   @ViewChild('scrollContainer', { static: true })
   scrollContainer!: ElementRef<HTMLElement>;
 
+  @ViewChild('prevArrow')
+  prevArrow?: ElementRef<HTMLElement>;
+
+  @ViewChild('nextArrow')
+  nextArrow?: ElementRef<HTMLElement>;
+
   get containerWidth(): number {
     return this.scrollContainer.nativeElement.offsetWidth;
   }
@@ -40,6 +46,41 @@ export class ChipsComponent {
   public next(): void {
     this.offset = Math.min(
       this.offset + this.containerWidth,
+      this.scrollContainerWidth - this.containerWidth
+    );
+  }
+
+  public focus(e: FocusEvent): void {
+    const element: HTMLElement = e.target as HTMLElement;
+
+    const prevArrowWidth: number =
+      this.prevArrow?.nativeElement.offsetWidth ?? 0;
+
+    const nextArrowWidth: number =
+      this.nextArrow?.nativeElement.offsetWidth ?? 0;
+
+    const newStartOffset: number = Math.max(
+      element.offsetLeft - prevArrowWidth,
+      0
+    );
+
+    const newEndOffset: number =
+      Math.min(
+        element.offsetLeft + element.offsetWidth + nextArrowWidth,
+        this.scrollContainerWidth
+      ) - this.containerWidth;
+
+    this.offset =
+      newStartOffset < this.offset
+        ? newStartOffset
+        : newEndOffset > this.offset
+        ? newEndOffset
+        : this.offset;
+  }
+
+  public wheel(e: WheelEvent): void {
+    this.offset = Math.min(
+      Math.max(0, this.offset + e.deltaY),
       this.scrollContainerWidth - this.containerWidth
     );
   }
